e0fa0dee06f8ccd579b65a06277dce8a14db0fbb
Lokesh Shivamurthy <lokesh.shivamurthy@trigyn.com>
2023-11-07 15:23:51 +0530

    changes in repository clone shell script

--- shell_scripts/repository_clone.sh
+++ shell_scripts/repository_clone.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 MY_PATH=microservices
-REPOSITORY=staging
+REPOSITORY=release-v5.0.3-nvsk-dic
 
 cd "$MY_PATH"
 
 if ! [[ -d spec-ms ]]; then
-	git clone https://github.com/Sunbird-cQube/spec-ms.git
+	git clone https://github.com/dicdiksha/spec-ms.git
 	cd "spec-ms"
-	git checkout $REPOSITORY
+	git checkout release-v5.0.3-nvsk
 	cd ../../
 else
 	cd "spec-ms"
 	git stash
-	git checkout $REPOSITORY
+	git checkout release-v5.0.3-nvsk
 	git pull
         cd ../../
 fi
 
 cd "$MY_PATH"
 if ! [[ -d ingestion-ms ]]; then
-	git clone https://github.com/Sunbird-cQube/ingestion-ms.git
+	git clone https://github.com/dicdiksha/ingestion-ms.git
 	cd "ingestion-ms"
-	git checkout $REPOSITORY
+	git checkout release-v5.0.3-nvsk
 	cd ../../
 else
 	cd "ingestion-ms"
 	git stash
-	git checkout $REPOSITORY
+	git checkout release-v5.0.3-nvsk
 	git pull
 	cd ../../
 fi
 
 cd "$MY_PATH"
 if ! [[ -d generator-ms ]]; then
-	git clone https://github.com/Sunbird-cQube/generator-ms.git
+	git clone https://github.com/dicdiksha/generator-ms.git
 	cd "generator-ms"
 	git checkout $REPOSITORY
 	cd ../../
@@ -51,12 +51,12 @@ cd "$MY_PATH"
 if ! [[ -d dashboard-ms ]]; then
         git clone https://github.com/dicdiksha/dashboard-ms.git
         cd "dashboard-ms"
-        git checkout release-v5.0.3-nvsk-dic
+        git checkout $REPOSITORY
         cd ../../
 else
         cd "dashboard-ms"
 	git stash
-        git checkout release-v5.0.3-nvsk-dic
+        git checkout $REPOSITORY
         git pull
         cd ../../
 
@@ -66,12 +66,12 @@ cd "$MY_PATH"
 if ! [[ -d query-builder ]]; then
         git clone https://github.com/dicdiksha/query-builder.git
         cd "query-builder"
-        git checkout release-v5.0.3-nvsk-dic
+        git checkout $REPOSITORY
         cd ../../
 else
         cd "query-builder"
 	git stash
-        git checkout release-v5.0.3-nvsk-dic
+        git checkout $REPOSITORY
         git pull
         cd ../../
 
